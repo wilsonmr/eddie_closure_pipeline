d5979707e99e5e37cef835af3812516840a26320
wilsonm <michael.wilson@ed.ac.uk>
2020-12-04 12:33:26 +0000

    increase run time of post, add upload if postfit success remove redudnant upload script

--- templates/post.sh
+++ templates/post.sh
@@ -7,7 +7,7 @@
 #set cwd (~/<job_id>)
 #$ -cwd
 #resources (time and memory) per replica
-#$ -l h_rt=01:30:00
+#$ -l h_rt=02:00:00
 #$ -l h_vmem=8G
 # email on abort (a) and end (e) so we know when to log back onto eddie
 #$ -m ae
@@ -24,3 +24,11 @@ source user_settings.ini
 evolven3fit <FITNAME> $END_FIT_REP
 # run postfit - bypass integrability for now.
 postfit $TARGET_REPS <FITNAME> --integrability-threshold $INT_THRESH
+
+# try uploading - only works if ssh key is on eddie
+if [ $? -eq 0 ]; then
+    echo "post fit successful: uploading"
+    vp-upload <FITNAME>
+else
+    echo "postfit failed"
+fi
